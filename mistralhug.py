b99eddd885737575226b4858483a84657a8ddc76
GammA <deltareally@gmail.com>
2025-03-08 10:57:43 +0100

    eos instruction

--- mistralhug.py
+++ mistralhug.py
@@ -1,3 +1,5 @@
+#source /home/alphaos/doc/mistralprj/mistral-env/bin/activate
+
 import torch
 from transformers import AutoTokenizer, AutoModelForCausalLM, BitsAndBytesConfig
 
@@ -7,14 +9,21 @@ tokenizer = AutoTokenizer.from_pretrained(model_id)
 
 model = AutoModelForCausalLM.from_pretrained(
     model_id,
-    device_map="auto",
-    quantization_config=BitsAndBytesConfig(
-        load_in_8bit=True,  # <-- Using 8-bit quantization
-    ),
+    torch_dtype=torch.bfloat16,  # or torch.float16
+    device_map="auto"
 )
 
-prompt = "Hi how are you?"
+prompt = "[INST] Hi, how are you? [/INST]"
 inputs = tokenizer(prompt, return_tensors="pt").to("cuda")
-output = model.generate(**inputs, max_new_tokens=150)
-print(tokenizer.decode(output[0], skip_special_tokens=True))
+outputs = model.generate(
+    **inputs,
+    do_sample=True,
+    temperature=0.5,
+    top_p=0.7,
+    max_new_tokens=50,
+    eos_token_id=tokenizer.eos_token_id,# Stops generation here
+    pad_token_id=tokenizer.eos_token_id  # longer detailed outputs
+)
+
+print(tokenizer.decode(outputs[0], skip_special_tokens=True))
 
